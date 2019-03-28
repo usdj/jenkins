725cbfafb366bc020423846577db3031ea41f2ca
jerrydeng <djy@yala-china.com>
2019-03-28 13:07:15 +0800

    init

---
+++ src/main/java/com/usdj/jenkins/JenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.usdj.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsApplication.class, args);
+	}
+
+}
